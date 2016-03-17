2ca375ee688cbd4c30beb566014ac0e79f22b524
Ömür Buruk <burukomur@gmail.com>
2016-03-17 18:46:26 +0200

    Roman rakam
    Dönüşümler

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.Location = new System.Drawing.Point(132, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ÇEVİR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Red;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(125, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Red;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(126, 153);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(105, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Not: Roman Rakamlarını Yalnızca Büyük Harf  Giriniz..";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(2, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Dönüştürülecek Değer :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(3, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Dönüşen Değer            :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(335, 284);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] sayısaldeger = { 1000, 500, 100, 50, 10, 5, 1 };
+        char[] romansaldeger = { 'M', 'D', 'C', 'L', 'X', 'V', 'I' };
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textekisayi = textBox1.Text;
+            int kontrol=0;
+            for (int p = 0; p < romansaldeger.Length; p++)
+            {
+                if (textekisayi[1] == romansaldeger[p])
+                    kontrol = 1;// Bu karşılaştırma sayının roman sayısı mı yoksa 10 tabanında sayımı 
+                // olduğunu kontrol eder
+                // işlemleri buna göre yapıcaz
+            }
+            if(kontrol==1)
+                textBox2.Text = Convert.ToString(Romanisayiya(textekisayi));
+            else
+                Sayiyiromana(Convert.ToInt32(textekisayi));
+        }
+        public int Romanisayiya(string roman)
+        {
+            textBox2.Text = "";
+            int[] romaninsayiya = new int[roman.Length] ;
+            int sayi = 0;
+            for (int i = 0; i < roman.Length; i++)
+            {
+                for (int j = 0; j < romansaldeger.Length; j++)
+                {
+                    if (roman[i] == romansaldeger[j])// gelen roman sayı değerleri ile roman sayı
+                    {//dizimizdeki değerler karşılaştırılıp sayısal karşılıkları aynı indis değeri
+                        romaninsayiya[i] = sayısaldeger[j];//ile sayısaldeger dizisinden alınır.
+                    }// ve değerlendirilmek üzere dizimize ayrı ayrı atılır.
+                }
+            }
+            for (int k = 0; k < romaninsayiya.Length; k++)
+            {
+                if (k != romaninsayiya.Length-1)
+                {
+                    if (romaninsayiya[k] < romaninsayiya[k + 1])
+                        sayi -= romaninsayiya[k];//dizimiz değerlendirmeye tabi tutulur sıralı bir 
+                    else// karşılaştırma yaptığımız için bize kolaylık sağlar 
+                        sayi += romaninsayiya[k]; // değerlerin büyüklük küçüklüğüne göre toplanır
+                }// yada çıkarılır .
+                else sayi += romaninsayiya[k];
+            }
+            return sayi;
+        }
+        public  void  Sayiyiromana(int sayi)
+        {
+            textBox2.Text = "";
+            int enuzunromansayisiuzunlugu = 15;
+            for (int u = 0; u < enuzunromansayisiuzunlugu; u++)
+            {// en fazla 15 kez döner döngümüz en uzun roman sayısı ...
+                if (sayi >= 1000)
+                {
+                    sayi -= 1000;
+                    textBox2.Text+= "M";
+                }
+                else if (sayi >= 900)
+                {
+                    sayi -= 900; ;
+                    textBox2.Text += "CM";
+                }
+                else if (sayi >= 500)
+                {
+                    sayi -= 500;
+                    textBox2.Text += "D";
+                }
+                else if (sayi >= 400)
+                {
+                    sayi -= 400;
+                    textBox2.Text += "CD";
+                }
+                else if (sayi >= 100)
+                {
+                    sayi -= 100;
+                    textBox2.Text += "C";
+                }
+                else if (sayi >= 90)
+                {
+                    sayi -= 90;
+                    textBox2.Text += "XC";
+                }
+                else if (sayi >= 50)
+                {
+                    sayi -= 50;
+                    textBox2.Text += "L";
+                }
+                else if (sayi >= 40)
+                {
+                    sayi -= 40;
+                    textBox2.Text += "XL";
+                }
+                else if (sayi >= 10)
+                {
+                    sayi -= 10;
+                    textBox2.Text += "X";
+                }
+                else if (sayi >= 9)
+                {
+                    sayi -= 9;
+                    textBox2.Text += "IX";
+                }
+                else if (sayi >= 5)
+                {
+                    sayi -= 5;
+                    textBox2.Text += "V";
+                }
+                else if (sayi >= 4)
+                {
+                    sayi -= 4;
+                    textBox2.Text += "IV";
+                }
+                else if (sayi >= 1)
+                {
+                    sayi -= 1;
+                    textBox2.Text += "I";
+                }
+                if (sayi == 0)// sayıdan çıkarılacak bişey kalmadığında döngüden çıkar
+                {
+                    break;
+                }
+            }
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+      
+      
+    }
+}
